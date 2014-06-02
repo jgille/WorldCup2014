4a87be67bca1a50fe91e08026cff8fb1e95da195
Jon Ivmark <jon.ivmark@klarna.com>
2014-06-02 23:04:50 +0200

    Save default team name

--- src/main/java/org/jon/ivmark/worldcup/server/DataStoreTeamRepository.java
+++ src/main/java/org/jon/ivmark/worldcup/server/DataStoreTeamRepository.java
@@ -3,7 +3,11 @@ package org.jon.ivmark.worldcup.server;
 import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.users.User;
 
+import java.util.logging.Logger;
+
 public class DataStoreTeamRepository implements TeamRepository {
+    private static final Logger LOGGER = Logger.getLogger(DataStorePlaysRepository.class.getName());
+
     private static final String KIND = "team";
 
     @Override
@@ -11,8 +15,12 @@ public class DataStoreTeamRepository implements TeamRepository {
         DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
         try {
             Entity entity = datastoreService.get(KeyFactory.createKey(KIND, user.getUserId()));
-            return (String) entity.getProperty("name");
+            String teamName = (String) entity.getProperty("name");
+            LOGGER.info("Team name: " + teamName + " for email " + user.getEmail());
+            return teamName;
         } catch (EntityNotFoundException e) {
+            setTeamName(user, user.getNickname());
+            LOGGER.info("Defaulting to team name: " + user.getNickname() + " for email " + user.getEmail());
             return user.getNickname();
         }
 
@@ -20,8 +28,11 @@ public class DataStoreTeamRepository implements TeamRepository {
 
     @Override
     public void setTeamName(User user, String teamName) {
+        LOGGER.info("Saving team name: " + teamName + " for email " + user.getEmail());
+
         DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
         Entity entity = new Entity(KIND, user.getUserId());
+        entity.setProperty("email", user.getEmail());
         entity.setUnindexedProperty("name", teamName);
         datastoreService.put(entity);
     }
